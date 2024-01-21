d59c9b2266dd9d3ffdb0d3ca708ec59f27642838
matuszmr <mati1900@interia.pl>
2024-01-21 12:18:25 +0100

    init

---
+++ README.md
@@ -0,0 +1,2 @@
+# [Mateusz Mróz](todo)
+My personal website hosted as GitHub Page.
---
+++ data.js
@@ -0,0 +1,246 @@
+export const bio = [
+    "I am a software tester with several years of experience. I have had the pleasure of working on a variety of projects, including European funded initiatives. I am a meticulous and detail-oriented person who learns quickly and adapts efficiently to new projects and technologies.",
+];
+
+// export const skills = [
+//     {
+//         title: "Languages",
+//         skillName: "JavaScript, TypeScript",
+//         color: "1",
+//         percentage: "80",
+//     },
+//     {
+//         title: "Frameworks/Libraries",
+//         skillName: "Angular, RxJS, NGXS",
+//         color: "6",
+//         percentage: "70",
+//     },
+//     {
+//         title: "Design",
+//         skillName: "HTML, Bootstrap, Tailwind",
+//         color: "4",
+//         percentage: "70",
+//     },
+//     {
+//         title: "Version Control",
+//         skillName: "GitHub, JIRA, Bitbucket",
+//         color: "7",
+//         percentage: "70",
+//     },
+//     {
+//         title: "Tools",
+//         skillName: "Postman, Chrome DevTools",
+//         color: "3",
+//         percentage: "80",
+//     },
+//     {
+//         title: "Analytics",
+//         skillName: "CleverTap, Countly",
+//         color: "5",
+//         percentage: "50",
+//     },
+//     {
+//         title: "IDE",
+//         skillName: "VS Code",
+//         color: "6",
+//         percentage: "70",
+//     },
+// ];
+
+// export const projects = {
+//     webProjects: [
+//         {
+//             projectName: "Programming Diaries",
+//             image: "images/programmingdiaries.png",
+//             summary:
+//                 "Developed a full stack blog application to provide content on techical topics across the internet with admin interface.",
+//             preview: "https://programmingdiaries.herokuapp.com/",
+//             techStack: ["Django", "SQLite", "Bootstrap", "JavaScript", "Heroku"],
+//         },
+//         {
+//             projectName: "Find Your Bank",
+//             image: "images/findyourbank.png",
+//             summary:
+//                 "Developed a React application to render a list of banks fetched from API. Filtered the banks based on queries from localstorage, marked favorites banks.",
+//             preview: "https://clever-fermi-0d5d76.netlify.app",
+//             techStack: ["Reactjs", "Bootstrap", "JavaScript", "Netlify"],
+//         },
+//         {
+//             projectName: "Web Portfolio",
+//             image: "images/portfolio.png",
+//             summary:
+//                 "Web Portfolio to showcase acadmics, skills, projects and contact details in better manner.",
+//             preview: "https://github.com/vinaysomawat/Travographer-Portal",
+//             techStack: ["HTML", "Bootstrap", "JavaScript"],
+//         },
+//         {
+//             projectName: "Resume Builder",
+//             image: "images/resume-builder.png",
+//             summary:
+//                 "Browser based editor to build and download Resumes in a customizable templates.",
+//             preview: "https://vinaysomawat.github.io/Resume-Builder",
+//             techStack: ["HTML", "Bootstrap", "JavaScript"],
+//         },
+//     ],
+//     softwareProjects: [
+//         {
+//             projectName: "Pizza Ordering ChatBot",
+//             image: "images/pizzaorderchatbot.png",
+//             summary:
+//                 "ChatBot using Dialogflow, Firebase database which stores the chat data in the realtime database.",
+//             preview: "https://github.com/vinaysomawat/Pizza-Ordering-ChatBot",
+//             techStack: ["Dailogflow", "Firebase"],
+//         },
+//         {
+//             projectName: "WhatsApp-Bot",
+//             image: "images/whatsappbot.jpg",
+//             summary:
+//                 "Python script which helps to send messages to WhatsApp contacts automatically using selenium and web automation.",
+//             preview: "https://github.com/vinaysomawat/WhatsApp-Bot",
+//             techStack: ["Selenium", "Chrome Webdriver", "Python"],
+//         },
+//         {
+//             projectName: "Bill Generator",
+//             image: "images/billgenerator.png",
+//             summary:
+//                 "GUI to transfer data to excel sheets and generate bills on the local shops.",
+//             preview: "https://github.com/vinaysomawat/Bill-Generator",
+//             techStack: ["Tkinter", "Openxlpy", "Python"],
+//         },
+//     ],
+//     androidProjects: [
+//         {
+//             projectName: "NITW-CSE",
+//             image: "images/nitwcse.jpg",
+//             summary:
+//                 "The Application display details of Department courses, reference books, research, publication and faculty profile.",
+//             preview: "https://github.com/vinaysomawat/NITW-CSE",
+//             techStack: ["JAVA", "XML", "Android"],
+//         },
+//         {
+//             projectName: "CareerHigh-App",
+//             image: "images/carrerhigh.png",
+//             summary:
+//                 "The Application display the webpages of website careerhigh.in in android devices.",
+//             preview: "https://github.com/vinaysomawat/CareerHigh-Android",
+//             techStack: ["JAVA", "XML", "Android"],
+//         },
+//     ],
+//     freelanceProjects: [
+//         {
+//             projectName: "SnylloAir.com",
+//             image: "images/snylloair.png",
+//             summary:
+//                 "Developed a company website to showcase the purpose, services and products provided by the company to audience.",
+//             preview: "https://www.snylloair.com/",
+//             techStack: ["Bootstrap", "JavaScript", "AWS-S3"],
+//         },
+//         {
+//             projectName: "Delivery+",
+//             image: "images/AM-Logo-.png",
+//             summary: "Android Application to display website in android devices.",
+//             preview:
+//                 "https://play.google.com/store/apps/details?id=com.americanmarket.americanmarketandroid",
+//             techStack: ["Android", "JAVA", "Play Store"],
+//         },
+//     ],
+// };
+
+export const experience = [
+    {
+        title: "EDEGETEQ Sp. z o.o.",
+        duration: "August 2018 - Present",
+        subtitle: "Senior Tester",
+        details: [
+            "Duties: TODO"
+        ],
+        tags: ["GIT", "GitLab", "Jira", "Postaman", "Sentry", "Cypress"],
+        icon: "rotate-right",
+    },
+    {
+        title: "Uniwersytet Kardynała Stefana Wyszyńskiego w Warszawie",
+        duration: "January 2023 - November 2023",
+        subtitle: "Senior Software Tester",
+        details: [
+            "Duties: TODO"
+        ],
+        tags: ["Postaman", "GitLab", "PostgeSQL"],
+        icon: "check",
+    },
+    {
+        title: "Telemedi Sp. z o.o.",
+        duration: "August 2021 – January 2022",
+        subtitle: "Senior Software Tester",
+        details: [
+            "Duties: TODO"
+        ],
+        tags: ["Postman", "Swagger", "Jira", "Confluence", "Xray", "GIT", "GCP"],
+        icon: "check",
+    },
+    {
+        title: "EDEGETEQ Sp. z o.o.",
+        duration: "June 2018 - October 2020",
+        subtitle: "Software Tester",
+        details: [
+            "Duties: TODO"
+        ],
+        tags: ["GitLab", "Jira", "DevTool", "Sentry"],
+        icon: "check",
+    },
+    {
+        title: "Medicover Sp. z o.o.",
+        duration: "August 2015 – July 2018",
+        subtitle: "Expert CIS/EMR",
+        details: [
+            "Duties: TODO"
+        ],
+        tags: [""],
+        icon: "check",
+    },
+];
+
+export const education = [
+    {
+        title: "University of Infrastructure and Management in Warsaw",
+        duration: "2010 - 2014",
+        subtitle: "Faculty: Management",
+        details: [],
+        tags: [],
+        icon: "graduation-cap",
+    },
+];
+
+export const footer = [
+    {
+        label: "Dev Profiles",
+        data: [
+            {
+                text: "GitHub",
+                link: "https://github.com/",
+            },
+            {
+                text: "LinkedIn",
+                link: "https://linkedin.com",
+            },
+        ],
+    },
+    {
+        label: "Resources",
+        data: [
+            {
+                text: "Enable Dark/Light Mode",
+                func: "enableDarkMode()",
+            },
+            {
+                text: "Print this page",
+                func: "window.print()",
+            },
+        ],
+    },
+    {
+        label: "copyright-text",
+        data: [
+            "My personal website hosted as GitHub Page",
+        ],
+    },
+];
